3a19778a46ec93a5d05e4c645da1c099e876700d
rostikcheremys <rostikcheremys@gmail.com>
2025-12-05 18:35:25 +0200

    feat: hook useSparks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
+import { useRef } from "react";
+
 import { SpinningDonut } from "../components/SpinningDonut/SpinningDonut.jsx";
 
-import "../styles/pages.css";
+import { useSparks } from "../hooks/useSparks.js";
 
 export const Home = () => {
+    const sparksRef = useRef(null);
+
+    useSparks(sparksRef, { count: 100, color: "var(--main-color)", size: 7, speed: 2 });
+
     return (
         <div className="page">
+            <div className="page__sparks" ref={sparksRef}></div>
+
             <label className="page__label-welcome">Want something sweet?</label>
 
             <SpinningDonut />
--- src/utils/validations/address.js
+++ src/utils/validations/address.js
@@ -1,3 +1,11 @@
 export const getAddressValidation = () => ({
+    minLength: {
+        value: 2,
+        message: "At least 2 characters"
+    },
+    maxLength: {
+        value: 50,
+        message: "Maximum 50 characters"
+    },
     required: "Address is required"
 });
