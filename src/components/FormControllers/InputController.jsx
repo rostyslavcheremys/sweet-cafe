0614bdb1d37c25de77bc3b260c951b67398ea1ab
rostikcheremys <rostikcheremys@gmail.com>
2025-12-08 23:50:05 +0200

    feat: add props className

--- src/components/FormControllers/InputController.jsx
+++ src/components/FormControllers/InputController.jsx
@@ -2,15 +2,16 @@ import { Controller } from "react-hook-form";
 
 import { FormField } from "../";
 
-export const InputController = ({ control, name, label, type, rules, disabled }) => {
+export const InputController = ({ control, name, label, type, className="",  defaultValue, rules, disabled }) => {
     return (
         <Controller
             name={name}
             control={control}
             rules={rules}
+            defaultValue={defaultValue}
             render={({ field, fieldState }) => (
                 <>
-                    <FormField label={label} type={type} {...field} disabled={disabled} />
+                    <FormField label={label} type={type} className={className} value={field.value ?? ""} {...field} disabled={disabled} />
                     {fieldState.error && <p className="form-controller__error">{fieldState.error.message}</p>}
                 </>
             )}
